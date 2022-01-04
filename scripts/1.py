863c66f2bf633f31e14510654703996160cba447
Ruo Ando <ando.ruo@gmail.com>
2022-01-04 13:21:32 +0900

    -lot

--- scripts/1.py
+++ scripts/1.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+
+args = sys.argv
  
-d = pd.read_csv('tmp',encoding = 'UTF8')
+d = pd.read_csv(args[1],encoding = 'UTF8')
 print(d)
 
-x = np.array(d["destination_ip"])
+x = np.array(d["source_ip"])
 y = np.array(d["count"])
 
 fig = plt.figure()
@@ -13,7 +16,7 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 ax.barh(x, y)
-ax.set_ylabel("destination_ip")
+ax.set_ylabel("source_ip")
 ax.set_xlabel("count")
 
 #ax.axes.yaxis.set_visible(False)
